8213dcf5c1500a0c9c309ec174b67307535098bb
mattymatty <mattymatty@freemails.it>
2025-12-20 15:40:09 +0100

    Update License to LGPL-2.1

--- Preloader/src/Attributes/HandleErrorsAttribute.cs
+++ Preloader/src/Attributes/HandleErrorsAttribute.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// MonkeyInjectionLibrary – A library mod that lets other mods add properties and methods to existing base-game classes at runtime, without modifying game files.
+// Copyright (C) 2025  mattymatty97
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+// USA
+
+using System;
 using JetBrains.Annotations;
 
 namespace InjectionLibrary.Attributes;
